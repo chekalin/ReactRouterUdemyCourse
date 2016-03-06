acb3595f3d24f2c2a021cac9e47b81443084a949
chekalin <stanislav.chekalin@gmail.com>
2016-03-06 07:23:43 +0000

    new post page

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <form className="form">
+                <input />
+            </form>
+        );
+    }
+}
+
+export default PostsIndex;
